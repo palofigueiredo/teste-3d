b9688c0533e7622724503a082efb3bb9d8112868
Jose_Cordeiro <e.jose.cordeiro@softfinanca.com>
2024-01-16 17:30:37 +0000

    iframe interativo e posicionado sobre os elementos do WebGL

--- main.js
+++ main.js
@@ -4,92 +4,62 @@ import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 
-
-
 // DIMENSIONS
 const sizes = {
     width: window.innerWidth,
     height:window.innerHeight
 }
-
 const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({alpha:true});
+
+const renderer = new THREE.WebGLRenderer();
 // const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0x123456,0.0)
-
-
-// renderer.setSize( 1024, 1024 );
 renderer.setSize( sizes.width, sizes.height);
 renderer.domElement.style.position = 'absolute'
 renderer.domElement.style.top = 0
-renderer.domElement.style.zIndex = 2;
-
-
+renderer.domElement.style.zIndex = 0;
 
 console.log(renderer.getSize)
 
+// document.body.appendChild( renderer.domElement ); //Only if the WebGL renderer is the one that'll contain the 3DCSS renderer
 
-
-
-
-// document.body.appendChild( renderer.domElement );
-
+//CSS3d Renderer
 const renderercss3d = new CSS3DRenderer();
 renderercss3d.setSize( sizes.width, sizes.height );
-// renderercss3d.setSize( 200,200 );
 renderercss3d.domElement.style.position = 'absolute'
 
-renderercss3d.domElement.appendChild(renderer.domElement)
+// renderercss3d.domElement.appendChild(renderer.domElement)
 
-
-
+document.body.appendChild( renderer.domElement );
 document.body.appendChild( renderercss3d.domElement );
+
+
+const group = new THREE.Group
 
 const geometry = new THREE.BoxGeometry(1, 1, 1); 
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
 // const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: false});
 const cube = new THREE.Mesh( geometry, material ); 
 scene.add( cube );
+// cube.position.x=0;
 cube.position.y=.5;
-// cube.position.y=0;
 cube.position.z=0.5;
 
-//CONTROLS
-
-// const controls = new OrbitControls(camera, scene)
 
 
-
-// Testing
-// const sphere = new THREE.SphereGeometry(.1)
+// TESTING
 const sphere = new THREE.SphereGeometry(0.1)
 const refball = new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({color:0x00ffff}))
-// refball.position.x = 0;
-// refball.position.y = 0;
-// refball.position.z = 0;
 
-const povfront = new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({color:0xff00ff}))
-scene.add(povfront)
-povfront.position.x = cube.position.x
+// const povfront = new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({color:0xff00ff}))
+// scene.add(povfront)
+// povfront.position.x = cube.position.x
 // povfront.position.y = cube.position.x + geometry.height/2
 
-console.log('cube.width: ')
-console.log(cube.x)
-
-
-
 scene.add(refball);
-scene.add(camera)
-// camera.position.x = 1;
-camera.position.z = 2;
-camera.position.y = 1.3;
-camera.position.x = .88;
 
-// camera.position.set(1,1,1) 
-camera.lookAt(cube.position);
 
 const displaygeom = new THREE.PlaneGeometry(0.5,0.5);
 // const display = new THREE.Mesh(displaygeom, new THREE.MeshBasicMaterial({color:0xdd00dd}))
@@ -104,6 +74,16 @@ scene.add(display)
 // End of testing
 
 
+//CAMERA
+scene.add(camera)
+camera.position.z = 2;
+camera.position.y = 1.3;
+camera.position.x = .88;
+
+// camera.position.set(1,1,1) 
+camera.lookAt(cube.position);
+
+
 const grid = new THREE.GridHelper();
 scene.add(grid);
 
@@ -115,53 +95,40 @@ div3d.id="div3d";
 // div3d.style.width = '4px';
 // div3d.style.height = '3px';
 div3d.style.width = 958;
+div3d.style.pointerEvents = 'auto'
 // div3d.style.height = sizes.height;
 console.log('sizes.width: ')
 console.log(sizes.width)
 div3d.style.backgroundColor = '#ff121233';
 // div3d.style.zIndex = 2
 
-
 const iframe = document.createElement( 'iframe' );
 iframe.style.width = 958;
 // iframe.style.height = '3px';
 iframe.style.border = '1px solid black';
 iframe.style.zIndex = 2;
+iframe.style.pointerEvents = 'auto';
 iframe.src = './ex-iframe.html';
 div3d.appendChild( iframe );
-// div3d.innerHTML += `<h1>I'm a div</h1>`
 
-// const object3d = new CSS3DObject( 'div' );
+
 const object3d = new CSS3DObject( div3d );
-// object3d.position.x = 0.5;
-// object3d.position.y = 0.5;
-// object3d.position.z = 0.5;
-// object3d.scale.set()
 
-// object3d.position = (0,0,0)
 
-// console.log('object3d.rotation: ')
-// console.log(object3d.rotation)
-// console.log('object3d.position: ')
-// console.log(object3d.position)
-// console.log('object3d scale: ')
-// console.log(object3d.scale)
-
-object3d.scale.set(2/sizes.width,2/sizes.width)
+object3d.scale.set(2/sizes.height,2/sizes.height)
 // object3d.scale.set(1,1)
 
 object3d.position.set( 0, 1, 1 );
 
+// scene.add(object3d)
 
-// object3d.rotation.set = (0,1,0);
 
-// const object2 = new CSS3DObject(Element.div)
+group.add(object3d)
+group.add(cube)
+group.add(display)
 
-scene.add(object3d)
 
-// const group = new THREE.Group();
-// group.add(object3d)
-// scene.add(group)
+scene.add(group)
 
 
 
@@ -174,20 +141,45 @@ scene.add(object3d)
 // End of inserts from cube
 
 
+//CONTROLS
+// const controls = new OrbitControls(camera, scene)
+// OrbitControls are not working
+// const cursor = {
+//     x:0,
+//     y:0
+// }
+// window.addEventListener('mousemove', (event) => { 
+//     cursor.x=2*event.clientX / sizes.width - 0.5
+//     cursor.y= - 3*(event.clientY / sizes.height - 0.5)
+//     console.log(cursor.x + ' , ' + cursor.y)
+//     })
+// Camera is updated in animate()
+
 
 
 function animate() {
-    
-    // cube.rotation.x += 0.001;
-	// cube.rotation.y += 0.01;
     camera.lookAt(cube.position);
 	requestAnimationFrame( animate );
-    renderercss3d.render( scene, camera );
+    
     renderer.render( scene, camera );
+    renderercss3d.render( scene, camera );
+    
     // controls.update();
-}
 
+//Update Camera
+    // camera.position.x = cursor.x*10
+    // camera.position.y = cursor.y*10
+    
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    
+    // camera.lookAt(new THREE.Vector3())
+    // camera.lookAt(cube.position)
+//End of Update Camera
+}
 animate();
+
 
 // Nav btns
 document.querySelector('.nav').addEventListener('click', event => {
@@ -197,26 +189,17 @@ document.querySelector('.nav').addEventListener('click', event => {
 })
 
 function rotateMesh (str) {
-    // if (str === '1') gsap.to(cube.rotation, {x: 0, y: 1, duration: 1});
-    // if (str === '2') gsap.to(cube.rotation, {x: 1, y: 0, duration: 1});
-    // if (str === '3') gsap.to(cube.rotation, {x: 0, y: 0, duration: 1});
 
-    if (str === '0') {
-        // camera.position.z = 2;
-        // camera.position.y = 1.3;
-        // camera.position.x = .88;
-        
-        gsap.to(camera.position,{x:.88, y:1.3, z:2,duration:.6});
-        
+    if (str === '0') { //Reset
+        gsap.to(camera.position,{x:.88, y:1.3, z:2,duration:.6});        
     }
-    if (str === '1') {
+    if (str === '1') { // Front
         gsap.to(camera.position, {x: cube.position.x, y: cube.position.y, z:cube.position.z+2, duration: 1});
     }
-    if (str === '2') {
+    if (str === '2') { // Top
         gsap.to(camera.position, {x: cube.position.x, y: cube.position.y+2, z:cube.position.z, duration: 1});
-        
     }
-    if (str === '3') {
+    if (str === '3') { // Side
         gsap.to(camera.position, {x: cube.position.x+2, y: cube.position.y, z:cube.position.z, duration: 1});
     }
 }
