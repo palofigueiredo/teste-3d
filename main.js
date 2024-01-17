db0666db4fb4bee96ed6a8fa951f9e011553a80f
Jose_Cordeiro <e.jose.cordeiro@softfinanca.com>
2024-01-17 11:59:36 +0000

    Some minor changes in code. Activated the camera "free" movement via mouse dragging through the screen.

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const sizes = {
     width: window.innerWidth,
     height:window.innerHeight
 }
+
 const scene = new THREE.Scene(); // Where the objects will be placed in order to be displayed
 
 // GRID for reference for a "ground" (horizontal plane at y=0)
@@ -48,10 +49,12 @@ const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true})
 const cube = new THREE.Mesh( boxgeometry, material ); 
 scene.add( cube );
 
+
 //Positioning the cube slightly in front of the origin (0,0,0)
 // cube.position.x=0;
 cube.position.y=.5;
 cube.position.z=0.5;
+
 
 // Creating a plane that will simulate the area where we want to place the display 
 const displaygeom = new THREE.PlaneGeometry(0.5,0.5);
@@ -63,6 +66,14 @@ displaywebgl.rotateX(-0.4)
 
 scene.add(displaywebgl)
 
+const displaygeom2 = new THREE.PlaneGeometry(0.5,0.5);
+const displaywebgl2 = new THREE.Mesh(displaygeom2, new THREE.MeshBasicMaterial({color:0x224466}))
+displaywebgl2.position.y=1
+displaywebgl2.position.z=0.5
+displaywebgl2.rotateX(4)
+
+scene.add(displaywebgl2)
+
 
 // Reference
 const sphere = new THREE.SphereGeometry(0.05)
@@ -73,16 +84,15 @@ scene.add(axesHelper)
 
 
 
-
-
-// Creating elements that will be placed on the CSS3D Renderer
+// Creating elements that will be placed in the CSS3D Renderer
 const div3d = document.createElement( 'div' );
 div3d.id="div3d";
+div3d.style.width = '300px'
 // div3d.style.pointerEvents = 'auto' // not needed, but was recommended to make sure elements in the div will be clickable
 div3d.style.backgroundColor = '#ff111133'; // red-ish with transparency
 
 const iframe = document.createElement( 'iframe' );
-// iframe.style.width = 958;
+iframe.style.width = '300px';
 // iframe.style.height = '3px';
 iframe.style.border = '1px solid black';
 iframe.style.zIndex = 2;
@@ -93,9 +103,9 @@ div3d.appendChild( iframe );
 
 const object3d = new CSS3DObject( div3d );
 
-object3d.scale.set(2/sizes.height,2/sizes.height)
-object3d.scale.set(0.005,0.005)
-// object3d.scale.set(1,1)
+// object3d.scale.set(1/(sizes.width),1/(sizes.width))
+// object3d.scale.set(0.005,0.005)
+object3d.scale.set(1/300,1/300)
 
 object3d.position.set( 0, 1, 1 );
 object3d.rotateX(-0.5)
@@ -110,28 +120,28 @@ scene.add(group)
 
 
 //CAMERA
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000); // the camera is where the "viewer" is. Has the parameters (vertical angle, aspect ratio, min distance for viewing something, maximum distance for viewing something - objects farther away will not be displayed)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 10000); // the camera is where the "viewer" is. Has the parameters (vertical angle, aspect ratio, min distance for viewing something, maximum distance for viewing something - objects farther away will not be displayed)
 scene.add(camera)
 camera.position.z = 2;
 camera.position.y = 1.3;
 camera.position.x = .88;
-
 camera.lookAt(cube.position);
 
 
-// CONTROLS (if you want to freely move the camera de-select the lines below and also the "Mouse camera controls" inside the animate() function right below)
-// const controls = new OrbitControls(camera, scene)
-// OrbitControls are not working
-// const cursor = {
-//     x:0,
-//     y:0
-// }
-// window.addEventListener('mousemove', (event) => { 
-//     cursor.x=2*event.clientX / sizes.width - 0.5
-//     cursor.y= - 3*(event.clientY / sizes.height - 0.5)
-//     console.log(cursor.x + ' , ' + cursor.y)
-//     })
-// // Camera is updated in animate()
+// CONTROLS 
+// // const controls = new OrbitControls(camera, scene)
+// // OrbitControls are not working
+// (if you want to freely move the camera de-select the lines below and also the "Mouse camera controls" inside the animate() function right below)
+const cursor = {
+    x:0,
+    y:0
+}
+window.addEventListener('mousemove', (event) => { 
+    cursor.x=2*event.clientX / sizes.width - 0.5
+    cursor.y= - 3*(event.clientY / sizes.height - 0.5)
+    console.log(cursor.x + ' , ' + cursor.y)
+    })
+// // Obs: Camera is updated in animate()
 
 
 
@@ -143,16 +153,16 @@ function animate() {
     renderercss3d.render( scene, camera );
     
     //Mouse camera controls (to activate, de-comment everything below and also the "controls" section just above
-    // controls.update();
-    // //Update Camera
-    // camera.position.x = cursor.x*10
-    // camera.position.y = cursor.y*10
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    // camera.position.y = cursor.y * 5
-    // camera.lookAt(new THREE.Vector3())
-    // camera.lookAt(cube.position)
-    // //End of Update Camera
+    // // controls.update();
+    // //Update Camera (manual camera movement - to activate deselect lines below)
+    camera.position.x = cursor.x*10
+    camera.position.y = cursor.y*10
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * 5
+    camera.lookAt(new THREE.Vector3())
+    camera.lookAt(cube.position)
+    //End of Update Camera
 }
 animate();
 
